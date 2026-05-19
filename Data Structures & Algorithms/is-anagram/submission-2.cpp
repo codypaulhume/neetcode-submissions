aceb5623143319ac82bd67cbbc0451b93a1a4314
Cody Hume <64334206+codypaulhume@users.noreply.github.com>
2026-05-18 20:00:14 -0600

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // Check length. If not the same - Immediately return false
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        // Create two maps. Log frequency of each.
+        unordered_map<char, int> myMap;
+        unordered_map<char, int> myMapTwo;
+        for(char c : s){
+            if(myMap.find(c) != myMap.end()){
+                myMap[c]++;
+            }else{
+                myMap[c] = 1;
+            }
+        }
+        for(char c : t){
+            if(myMap.find(c) != myMap.end()){
+                myMapTwo[c]++;
+            }else{
+                myMapTwo[c] = 1;
+            }
+        }
+
+        // Compare the two maps and return the value
+        return myMap == myMapTwo;
+    }
+};
